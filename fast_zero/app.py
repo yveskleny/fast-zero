e296d70f0420b27e518c79db92e1e2fb3acd3e34
Yves Kleny <yves.kleny.1@gmail.com>
2024-12-18 10:03:53 -0300

    Aula 02 - Fundamentos do desenvolvimento web

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,26 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Hello, World!"}
+
+
+@app.get("/hello", status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def hello_world():
+    return """
+    <html>
+      <head>
+        <title> Our Hello World!</title>
+      </head>
+      <body>
+        <h1> Hello World </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,12 @@ def test_read_root_deve_retornar_ok_e_hello_word():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Hello, World!"}
+
+
+def test_hello_world_deve_retornar_hello_world_html():
+    client = TestClient(app)
+
+    response = client.get("/hello")
+
+    assert response.status_code == HTTPStatus.OK
+    assert "<h1> Hello World </h1>" in response.text
